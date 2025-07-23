5c52a2eaf3aedd60736adb26c80cd221fa591a21
Canaan-HS <s3459897@gmail.com>
2025-07-23 17:44:24 +0800

    log_regex

---
+++ Modules/utils/log_regex.py
@@ -0,0 +1,22 @@
+from ..bootstrap import re
+
+LOG_PATTERNS = (
+    (re.compile(r"(\d{4}\.\d{2}\.\d{2} \d{2}:\d{2}:\d{2}).*?Played Map:\s*([^,]+)"), "map"),
+    (re.compile(r"(\d{4}\.\d{2}\.\d{2} \d{2}:\d{2}:\d{2}).*?Slasher:\s*(\d+)"), "slasher"),
+    (
+        re.compile(
+            r"(\d{4}\.\d{2}\.\d{2} \d{2}:\d{2}:\d{2}).*?Selected Items:\s*(.+?)(?=,\s*\w+:|$)"
+        ),
+        "items",
+    ),
+    (
+        re.compile(
+            r"(\d{4}\.\d{2}\.\d{2} \d{2}:\d{2}:\d{2}).*?SC_(generator\d+) Progress check\. Last (\w+) value: (.*?), updated (\w+) value: (.*)"
+        ),
+        "generator",
+    ),
+    (
+        re.compile(r"(\d{4}\.\d{2}\.\d{2} \d{2}:\d{2}:\d{2}).*?Generators reset(?: again)?\."),
+        "reset",
+    ),
+)
