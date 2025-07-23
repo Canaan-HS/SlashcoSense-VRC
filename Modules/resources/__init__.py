2f13cd4894dcd7eef3bdb45dac4d756b31a4f731
Canaan-HS <s3459897@gmail.com>
2025-07-23 17:40:31 +0800

    mappings 模塊索引調整

--- Modules/resources/__init__.py
+++ Modules/resources/__init__.py
@@ -1,4 +1,4 @@
 from .mappings.Slashers import SLASHERS
 from .mappings.Game_Maps import GAME_MAPS
 from .mappings.Items import ITEMS, ITEMS_PATTERN
-from .mappings.Progress_Color import PROGRESS_COLORS
+from .mappings.Progress_Color import GetProgressColor
