f394b376c85211fdcca3afbe256c5d4ae6ffa846
Canaan-HS <s3459897@gmail.com>
2025-07-23 17:31:52 +0800

    resources init

---
+++ Modules/resources/__init__.py
@@ -0,0 +1,4 @@
+from .mappings.Slashers import SLASHERS
+from .mappings.Game_Maps import GAME_MAPS
+from .mappings.items import ITEMS, ITEMS_PATTERN
+from .mappings.Progress_Color import PROGRESS_COLORS
