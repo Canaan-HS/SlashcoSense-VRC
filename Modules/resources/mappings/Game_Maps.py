97369295be5dbb4d6a13feffa93d9075b4c08d04
Canaan-HS <s3459897@gmail.com>
2025-07-23 18:01:23 +0800

    調整函數名稱 Transl -> transl

--- Modules/resources/mappings/Game_Maps.py
+++ Modules/resources/mappings/Game_Maps.py
@@ -1,14 +1,14 @@
-from ...language import Transl
+from ...language import transl
 
 GAME_MAPS = {
-    "0": Transl("舊 SlashCo 總部"),
-    "SlashCoHQ": Transl("舊 SlashCo 總部"),
-    "1": Transl("馬龍斯農場"),
-    "MalonesFarmyard": Transl("馬龍斯農場"),
-    "2": Transl("菲利普斯•書斯特伍德高中"),
-    "PhilipsWestwoodHighSchool": Transl("菲利普斯•書斯特伍德高中"),
-    "3": Transl("伊斯特伍德綜合醫院"),
-    "EastwoodGeneralHospital": Transl("伊斯特伍德綜合醫院"),
-    "4": Transl("德爾塔科研機構"),
-    "ResearchFacilityDelta": Transl("德爾塔科研機構"),
+    "0": transl("舊 SlashCo 總部"),
+    "SlashCoHQ": transl("舊 SlashCo 總部"),
+    "1": transl("馬龍斯農場"),
+    "MalonesFarmyard": transl("馬龍斯農場"),
+    "2": transl("菲利普斯•書斯特伍德高中"),
+    "PhilipsWestwoodHighSchool": transl("菲利普斯•書斯特伍德高中"),
+    "3": transl("伊斯特伍德綜合醫院"),
+    "EastwoodGeneralHospital": transl("伊斯特伍德綜合醫院"),
+    "4": transl("德爾塔科研機構"),
+    "ResearchFacilityDelta": transl("德爾塔科研機構"),
 }
--- Modules/resources/mappings/Slashers.py
+++ Modules/resources/mappings/Slashers.py
@@ -1,61 +1,61 @@
-from ...language import Transl
+from ...language import transl
 from ...bootstrap import ASSETS
 
 SLASHERS = {
     0: {
-        "name": Transl("巴巴布伊 【肌肉男 / 隱形怪】"),
+        "name": transl("巴巴布伊 【肌肉男 / 隱形怪】"),
         "icon": f"{ASSETS}/BABABOOEY.webp",
     },
     1: {
-        "name": Transl("席德 【手槍怪 / 餅乾怪】"),
+        "name": transl("席德 【手槍怪 / 餅乾怪】"),
         "icon": f"{ASSETS}/SID.webp",
     },
     2: {
-        "name": Transl("特羅勒格巨魔【笑臉男 / 火柴人】"),
+        "name": transl("特羅勒格巨魔【笑臉男 / 火柴人】"),
         "icon": f"{ASSETS}/TROLLGE.webp",
     },
     3: {
-        "name": Transl("博格梅爾【機器人】"),
+        "name": transl("博格梅爾【機器人】"),
         "icon": f"{ASSETS}/BORGMIRE.webp",
     },
     4: {
-        "name": Transl("阿博米納特【憎惡者 / 外星人】"),
+        "name": transl("阿博米納特【憎惡者 / 外星人】"),
         "icon": f"{ASSETS}/ABOMIGNAT.webp",
     },
     5: {
-        "name": Transl("口渴 【爬行者 / 牛奶怪】"),
+        "name": transl("口渴 【爬行者 / 牛奶怪】"),
         "icon": f"{ASSETS}/THIRSTY.webp",
     },
     6: {
-        "name": Transl("埃爾默神父 【霰彈槍 / 神父】"),
+        "name": transl("埃爾默神父 【霰彈槍 / 神父】"),
         "icon": f"{ASSETS}/FATHER_ELMER.webp",
     },
     7: {
-        "name": Transl("觀察者 【高個子】"),
+        "name": transl("觀察者 【高個子】"),
         "icon": f"{ASSETS}/THE_WATCHER.webp",
     },
     8: {
-        "name": Transl("野獸 【貓貓 / 貓老太】"),
+        "name": transl("野獸 【貓貓 / 貓老太】"),
         "icon": f"{ASSETS}/THE_BEAST.webp",
     },
     9: {
-        "name": Transl("海豚人"),
+        "name": transl("海豚人"),
         "icon": f"{ASSETS}/DOLPHINMAN.webp",
     },
     10: {
-        "name": Transl("伊戈爾【DJ / 創造者 / 毀滅者】"),
+        "name": transl("伊戈爾【DJ / 創造者 / 毀滅者】"),
         "icon": f"{ASSETS}/IGOR.webp",
     },
     11: {
-        "name": Transl("牢騷者【乞丐】"),
+        "name": transl("牢騷者【乞丐】"),
         "icon": f"{ASSETS}/THE_GROUCH.webp",
     },
     12: {
-        "name": Transl("公主【狗】"),
+        "name": transl("公主【狗】"),
         "icon": f"{ASSETS}/PRINCESS.webp",
     },
     13: {
-        "name": Transl("極速奔跑者"),
+        "name": transl("極速奔跑者"),
         "icon": f"{ASSETS}/SPEEDRUNNER.webp",
     },
 }
