c5e106af6003d26d0f411146b142b41b024e6854
Canaan-HS <s3459897@gmail.com>
2025-07-23 17:44:50 +0800

    增加 utils 模塊引用

--- Modules/__init__.py
+++ Modules/__init__.py
@@ -1,3 +1,4 @@
 from .bootstrap import *
+from .utils import LOG_PATTERNS
 from .language import Transl, Translator
 from .resources import ITEMS, ITEMS_PATTERN, SLASHERS, GAME_MAPS, GetProgressColor
