2b4c8135f9ea5de66ba105e5551076779a1b67e1
Canaan-HS <s3459897@gmail.com>
2025-07-23 17:39:45 +0800

    resources 模塊索引調整

--- Modules/__init__.py
+++ Modules/__init__.py
@@ -1,3 +1,3 @@
 from .bootstrap import *
 from .language import Transl, Translator
-from .resources import ITEMS, ITEMS_PATTERN, SLASHERS, GAME_MAPS, PROGRESS_COLORS
+from .resources import ITEMS, ITEMS_PATTERN, SLASHERS, GAME_MAPS, GetProgressColor
