0a431a55c5d4e65d3a09f0ada341eccb15cdaad3
Canaan-HS <s3459897@gmail.com>
2025-07-23 17:32:17 +0800

    Game_Maps

---
+++ Modules/resources/mappings/Game_Maps.py
@@ -0,0 +1,14 @@
+from ...language import Transl
+
+GAME_MAPS = {
+    "0": Transl("舊 SlashCo 總部"),
+    "SlashCoHQ": Transl("舊 SlashCo 總部"),
+    "1": Transl("馬龍斯農場"),
+    "MalonesFarmyard": Transl("馬龍斯農場"),
+    "2": Transl("菲利普斯•書斯特伍德高中"),
+    "PhilipsWestwoodHighSchool": Transl("菲利普斯•書斯特伍德高中"),
+    "3": Transl("伊斯特伍德綜合醫院"),
+    "EastwoodGeneralHospital": Transl("伊斯特伍德綜合醫院"),
+    "4": Transl("德爾塔科研機構"),
+    "ResearchFacilityDelta": Transl("德爾塔科研機構"),
+}
