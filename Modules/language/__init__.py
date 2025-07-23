7891b9fd4313e4bb07578f211d22e545ee1a73c8
Canaan-HS <s3459897@gmail.com>
2025-07-23 20:57:19 +0800

    1. 增加 translator 回傳, 設置語言 2. detect_system_language 分離

--- Modules/language/__init__.py
+++ Modules/language/__init__.py
@@ -17,32 +17,42 @@ Locale = {
 }
 
 
-def translator(lang=None):
-
-    Transl = {}
+def detect_system_language(lang) -> str:
+    """檢測系統語言"""
 
     try:
-        if lang is None:
-            if SysPlat == "Windows":
-                buffer = ctypes.create_unicode_buffer(85)
-                ctypes.windll.kernel32.GetUserDefaultLocaleName(buffer, len(buffer))
-                lang = buffer.value.replace("-", "_")
-            elif SysPlat in ("Linux", "Darwin"):
-                lang = os.environ.get("LANG", "").split(".")[0]
-            else:
-                locale.setlocale(locale.LC_ALL, "")
-                lang = locale.getlocale()[1].replace("cp", "")
+        if SysPlat == "Windows":
+            buffer = ctypes.create_unicode_buffer(85)
+            ctypes.windll.kernel32.GetUserDefaultLocaleName(buffer, len(buffer))
+            lang = buffer.value.replace("-", "_")
+        elif SysPlat in ("Linux", "Darwin"):
+            lang = os.environ.get("LANG", "").split(".")[0]
+        else:
+            locale.setlocale(locale.LC_ALL, "")
+            lang = locale.getlocale()[1].replace("cp", "")
     except Exception:
         lang = Default
 
-    Transl = (
-        Words.get(lang)
-        if isinstance(lang, str) and lang in Words
-        else Words.get(Locale.get(lang)) if lang in Locale else Words.get(Default)
-    )
+    return lang
 
-    return lambda text: Transl.get(text, text)
+
+def translator(lang=None):
+    """獲取翻譯器"""
+
+    Transl = {}
+    SetLang = ""
+
+    lang = detect_system_language(lang) if lang is None else lang
+
+    if isinstance(lang, str) and lang in Words:
+        Transl, SetLang = Words.get(lang), lang
+    elif lang in Locale:
+        Transl, SetLang = Words.get(Locale.get(lang)), Locale.get(lang)
+    else:
+        Transl, SetLang = Words.get(Default), Default
+
+    return (lambda text: Transl.get(text, text), SetLang)
 
 
 # 先使用自動檢測
-transl = translator()
+transl, setLang = translator()
