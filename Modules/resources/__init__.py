aa91d13c8137220fc94675c0cee892ad8784b10a
Canaan-HS <s3459897@gmail.com>
2025-07-23 18:12:38 +0800

    更改函數名稱 ParseItems -> parse_items

--- Modules/resources/__init__.py
+++ Modules/resources/__init__.py
@@ -1,5 +1,5 @@
 from .mappings.Slashers import SLASHERS
 from .mappings.Game_Maps import GAME_MAPS
 
-from .mappings.Items import ParseItems
+from .mappings.Items import parse_items
 from .mappings.Progress_Color import GetProgressColor
